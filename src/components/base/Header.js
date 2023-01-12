7390c7ca5bcdb604ae7a8c32523f7bb5e2aaa99e
gaeguriBanchan <rlawowls93@gmail.com>
2023-01-12 18:10:14 +0900

    메뉴 기본레이아웃 작업중

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,11 +1,13 @@
 /** @format */
 
-import { Outlet } from "react-router";
+import { Outlet } from 'react-router';
+
 
 const Header = () => {
   return (
     <>
-      <div className="w-full bg-red-500">헤더</div>
+      <div className='w-full bg-red-500 h-40'>
+      </div>
       <Outlet />
     </>
   );
