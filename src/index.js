e8f9d823a123a1310a8a53706a8845a1f1259c65
DG Yoon <douglasyoon9@gmail.com>
2023-01-12 11:54:27 +0900

    기본 설정 수정
    - 기본 설정 보완
    - react helmet async 적용 예시 업로드

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 /** @format */
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
