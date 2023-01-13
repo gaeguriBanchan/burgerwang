0d06a8f69fa72daff387a4fd32e04f5d980c3b0e
DG Yoon <douglasyoon9@gmail.com>
2023-01-13 15:18:40 +0900

    검은색 버튼 추가

---
+++ src/components/base/ActiveBlackButton.js
@@ -0,0 +1,9 @@
+const ActiveBlackButton = ({ name }) => {
+  return (
+    <button className="w-48 h-16 ml-1 bg-512314 text-white text-2xl font-black rounded-xl">
+      {name}
+    </button>
+  );
+};
+
+export default ActiveBlackButton;
