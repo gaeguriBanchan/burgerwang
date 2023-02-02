ca648911306c080a1357f3e00ba38dc15cdd8017
choi <bcaca0831@gmail.com>
2023-02-02 19:21:55 +0900

    머지

--- src/components/base/GoMypage.js
+++ src/components/base/GoMypage.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import man from '../../assets/ninggen.jpg';
-import { useNavigate } from 'react-router';
+import { useSelector, useDispatch } from "react-redux";
+import profile from "../../assets/images/icon_profile.jpg";
+import { useNavigate } from "react-router";
 
 const GoMypage = () => {
   // 김재진 : 타이틀 출력
   const navigate = useNavigate();
   const disptach = useDispatch();
   const goMypage = () => {
-    navigate('/mypage');
+    navigate("/mypage");
   };
   const userInfo = useSelector((state) => {
     return state.user;
   });
   return (
     <>
-      <div className='flex justify-between gap-5 self-center'>
-        <img src={man} alt='man' className='w-16 h-16 rounded-full' />
+      <div className="flex justify-between gap-5 items-center">
+        <div className="w-12 h-12">
+          <img src={profile} alt="man" className="rounded-full" />
+        </div>
         <div>
-          <p className='text-2xl'>김그린 님 안녕하세요</p>
+          <p className="text-xl text-white">김그린 님 안녕하세요</p>
           {/* 김재진 : 마이페이지이동 */}
-          <span className='text-2xl  cursor-pointer' onClick={goMypage}>
-            MY왕 바로가기 {'>'}
+          <span className="text-xl text-white font-black" onClick={goMypage}>
+            MY왕 바로가기 {">"}{" "}
           </span>
         </div>
       </div>
--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,32 +1,21 @@
 /** @format */
-
-// /** @format */
-
-import { Outlet } from 'react-router';
-import logo from '../../assets/logo.png';
-import man from '../../assets/ninggen.jpg';
-import { Link, useNavigate } from 'react-router-dom';
-
-import { Outlet, useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.png';
-import man from '../../assets/ninggen.jpg';
-import { useSelector, useDispatch } from 'react-redux';
+import { Outlet } from "react-router";
+import logo from "../../assets/images/logo.png";
+// import profile from "../../assets/images/icon_profile.jpg";
+import { Link, useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
 // 김재진 : 타이틀 출력
-import GoMypage from './GoMypage';
+import GoMypage from "./GoMypage";
 const Header = () => {
-  const navigate = useNavigate();
-  const linkMypage = () => {
-    navigate('/myPage');
-  };
-
-  
-  // 김재진 : 타이틀 출력
+  // const linkMypage = () => {
+  //   navigate("/myPage");
+  // };
   // const navigate = useNavigate();
+  // 김재진 : 타이틀 출력
   // const disptach = useDispatch();
   // const goMypage = () => {
-  //   navigate('/mypage');
-  //   disptach(setTitle({ title: '마이페이지' }));
+  //   navigate("/mypage");
+  //   disptach(setTitle({ title: "마이페이지" }));
   // };
   // const userInfo = useSelector((state) => {
   //   return state.user;
@@ -41,16 +30,21 @@ const Header = () => {
               <span className="font-JUA text-6xl text-white">버거왕</span>
             </div>
           </Link>
-          <div className="flex justify-between gap-5 items-center">
+          {/* <div className="flex justify-between gap-5 items-center">
             <div className="w-12 h-12">
               <img src={profile} alt="man" className="rounded-full" />
             </div>
             <div>
               <p className="text-xl text-white">김그린 님 안녕하세요</p>
-              <span className="text-xl text-white font-black" onClick={linkMypage}
-                style={{ cursor: 'pointer' }}>MY왕 바로가기 {">"}</span>
+              <span
+                className="text-xl text-white font-black"
+                onClick={linkMypage}
+                style={{ cursor: "pointer" }}
+              >
+                MY왕 바로가기 {">"}
+              </span>
             </div>
-          </div>
+          </div> */}
           {/* {userInfo.emil !== '' ? <GoMypage/>: ''} */}
           <GoMypage />
         </div>
